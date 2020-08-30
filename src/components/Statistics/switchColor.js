91cf00f5ee8ba95c27ed1f103552f53f56f37781
Aleksey Grygorenko <aleksey2892@gmail.com>
2020-08-30 23:11:36 +0300

    fix style path in switchColor

--- src/components/Statistics/switchColor.js
+++ src/components/Statistics/switchColor.js
@@ -1,16 +1,16 @@
-import styles from './Styles.module.scss';
+import { docx, pdf, mp3, psd, item } from './Styles.module.scss';
 
 export default function switchColor(label) {
   switch (label) {
     case '.docx':
-      return styles.docx;
+      return docx;
     case '.pdf':
-      return styles.pdf;
+      return pdf;
     case '.mp3':
-      return styles.mp3;
+      return mp3;
     case '.psd':
-      return styles.psd;
+      return psd;
     default:
-      return styles.item;
+      return item;
   }
 }
